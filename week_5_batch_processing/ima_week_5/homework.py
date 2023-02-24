be169789c9561948050c19cc67f2398301ed93e5
Baktagul Imasheva <DWtouMS8K7>
2023-02-25 00:42:22 +0600

    Week-5 homework update

--- week_5_batch_processing/ima_week_5/homework.py
+++ week_5_batch_processing/ima_week_5/homework.py
@@ -22,25 +22,23 @@ df = spark.read \
     .option("header", "true") \
     .csv('fhvhv_tripdata_2021-06.csv.gz')
 
-### Question 1 and 2
-# df = df.repartition(12)
-# df.write.parquet('fhvhv/2021/06/')
-#
-# print("Finished")
-#
+# Question 1 and 2
+df_partitioned = df.repartition(12)
+df_partitioned.write.parquet('fhvhv/2021/06/')
+print("Finished")
 
-### Question 3
-# row_cnt = df.filter(F.to_date(df.pickup_datetime) == "2021-06-15").count()
-# print(row_cnt)
-#
 
-### Question 4
-# df = df.withColumn("trip_duration", (df.dropoff_datetime.cast("long") - df.pickup_datetime.cast("long"))/3600) \
-#     .withColumn("pickup_date", F.to_date(df.pickup_datetime)) \
-#     .select("pickup_date", "trip_duration") \
-#     .groupBy("pickup_date").max("trip_duration") \
-#     .sort(F.desc("max(trip_duration)")) \
-#     .show()
+# Question 3
+row_cnt = df.filter(F.to_date(df.pickup_datetime) == "2021-06-15").count()
+print(row_cnt)
+
+# Question 4
+df.withColumn("trip_duration", (df.dropoff_datetime.cast("long") - df.pickup_datetime.cast("long")) / 3600) \
+    .withColumn("pickup_date", F.to_date(df.pickup_datetime)) \
+    .select("pickup_date", "trip_duration") \
+    .groupBy("pickup_date").max("trip_duration") \
+    .sort(F.desc("max(trip_duration)")) \
+    .show()
 
 # Question 6
 df_zone = spark.read \
@@ -55,5 +53,3 @@ df_agg = df.groupBy("PULocationID") \
 
 df_join = df_agg.join(df_zone, df_agg.PULocationID == df_zone.LocationID, "left")
 df_join.show(truncate=False)
-
-
