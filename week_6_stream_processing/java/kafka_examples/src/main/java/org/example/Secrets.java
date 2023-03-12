57d2e81187268f3943e4fc110f5d843322b56951
Baktagul Imasheva <DWtouMS8K7>
2023-03-13 00:29:02 +0600

    Change

--- week_6_stream_processing/java/kafka_examples/src/main/java/org/example/Secrets.java
+++ week_6_stream_processing/java/kafka_examples/src/main/java/org/example/Secrets.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Secrets {
-    public static final String KAFKA_CLUSTER_KEY = "3G4MTNXCQJQZFFA4";
-    public static final String KAFKA_CLUSTER_SECRET = "rORt0T4kPPNktwhHkPVsJVAw40o2bkGpsRvOVtkfaVFCOkZXOag0OqJuPpVdhx43";
+    public static final String KAFKA_CLUSTER_KEY = "NOT";
+    public static final String KAFKA_CLUSTER_SECRET = "NOT";
 
     public static final String SCHEMA_REGISTRY_KEY = "REPLACE_WITH_SCHEMA_REGISTRY_KEY";
     public static final String SCHEMA_REGISTRY_SECRET = "REPLACE_WITH_SCHEMA_REGISTRY_SECRET";
